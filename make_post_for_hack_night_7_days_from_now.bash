1ca86c3b6710b2183c7389c53150103fd14f02ef
gnarlinsky <gnarlinsky@gmail.com>
2014-12-30 21:16:05 -0600

    Script to generate next Tuesday's hack night post now does the upcoming Tuesday, not just seven days from current date.

--- make_post_for_hack_night_7_days_from_now.bash
+++ make_post_for_hack_night_7_days_from_now.bash
@@ -5,14 +5,18 @@
 #########################################################################
 # TODO: make post for next *Tuesday*, rather than just 7 days from now.
 
-# get today's date for the file name, e.g. 2014-11-11
-today=`date +"%Y-%m-%d"`
+## get today's date for the file name, e.g. 2014-11-11
+#today=`date +"%Y-%m-%d"`
 
-# take it to next week
-seven_days_from_today=`date -j -v +7d -f "%Y-%m-%d" $today +%Y-%m-%d`
+## take it to next week
+#seven_days_from_today=`date -j -v +7d -f "%Y-%m-%d" $today +%Y-%m-%d`
+
+# get next Tues from Python script
+next_tues=`python get_next_tues.py`
 
 # finish building the file's name, e.g. 2014-11-18-Hack-Session.md
-file_name=$seven_days_from_today"-Hack-Session.md"
+#file_name=$seven_days_from_today"-Hack-Session.md"
+file_name=$next_tues"-Hack-Session.md"
 
 # copy template text to file with file_name
 cp hack_night_post_text.txt _posts/$file_name
